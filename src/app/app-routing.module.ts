61968cfa2d948402cce55c79996a9b340b9f8526
jojo52807531 <97908088+jojo52807531@users.noreply.github.com>
2022-10-20 19:57:44 +0100

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProduitsComponent } from './produits/produits.component';
+import { AddProduitComponent } from './add-produit/add-produit.component';
+const routes: Routes = [{path: "produits", component : ProduitsComponent}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
